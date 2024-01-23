74c519837812ac48ee6f9dab85dc54e64f6c0a80
Ruben SL <ruben.lucio.si@gmail.com>
2024-01-23 12:49:12 -0600

    Se crea P3 y P2 is ok

--- Second/P2.c
+++ Second/P2.c
@@ -7,8 +7,8 @@ int main()
     printf("Esta aplicacion hara conversiones de cm, mm y km\n");
     printf("\nIntroduzca la cifra deseada a convertir (solo los numeros): \n");
     scanf("%d", & num);
-    printf("\nIntroduzca el numero que indique el  tipo de unidad de medida que esta usando 1->cm, 2->mm o 3->km): \n");
-    scanf("%d\n", & tipo);
+    printf("\nIntroduzca el numero que indique el  tipo de unidad de medida que esta usando 1->cm, 2->mm, 3->km, 4->met : \n");
+    scanf("%d", & tipo);
     
     switch (tipo)
     {
@@ -22,6 +22,9 @@ int main()
     case 3:
         gotKm(num);
         break;
+    case 4:
+        gotMet(num);
+        break;
     default:
         printf("ERROR!!!Introdujo mal el tipo de unidad de medida que tiene");
         break;
@@ -31,20 +34,37 @@ int main()
 
 void gotCM(int a){
     int mm = a *10;
-    float km = a / 100000;
-    printf("%d %f %d %f", "El valor que puso: "a, "a mm es: "mm);
-    printf("%d %f", a, km);
-    
+    float km = a / 100000.0;
+    float m = a /10.0;
+    printf("%dcm son: %dmm\n", a, mm);
+    printf("\n%dcm son: %.6fkm\n", a, km); 
+    printf("\n%dcm son: %.6fmet\n", a, m);
+
 }
+
 void gotMm(int a){
-    int cm = a/10;
-    float km = a/1000000;
-    printf("\n%dmm son: %dcm\n", a, cm);
-    printf("\n%dmm son: %fkm", a, km);
+    float cm = a/10.0;
+    float km = a/1000000.0;
+    float m = a /1000.0;
+    printf("\n%dmm son: %.6fcm\n", a, cm);
+    printf("\n%dmm son: %.6fkm\n", a, km);
+    printf("\n%dmm son: %.6fmet\n", a, m);
+
 }
 void gotKm(int a){
     int cm = a * 100000;
     int mm = a * 1000000;
+    int m = a *1000;
     printf("\n%dkm son: %dcm\n", a, cm);
-    printf("\n%dkm son: %dmm", a, mm);
+    printf("\n%dkm son: %dmm\n", a, mm);
+    printf("\n%dkm son: %dmet\n", a, m);
+
+}
+void gotMet(int a){
+    int cm = a *100;
+    int mm = a *1000;
+    float km = a /1000.0;
+    printf("\n%dmet son: %dcm\n", a, cm);
+    printf("\n%dmet son: %f.6km\n", a, km);
+    printf("\n%dmet son: %dmm\n", a, mm);
 }
