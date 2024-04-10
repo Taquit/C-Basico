645fa30f4d00cbf26d02da9959ccfd948a609d41
Ruben SL <ruben.lucio.si@gmail.com>
2024-04-10 16:35:48 -0600

    .2.1

---
+++ P30/cmake-build-debug/CMakeFiles/P30.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P30.dir/main.c.obj"
+  "CMakeFiles/P30.dir/main.c.obj.d"
+  "P30.exe"
+  "P30.exe.manifest"
+  "P30.pdb"
+  "libP30.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/P30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Proyecto-Almacen/Proyecto-Individual/main.c
+++ Proyecto-Almacen/Proyecto-Individual/main.c
@@ -3,41 +3,61 @@
 
 void menu(){
     printf("1)Altas\n2)Listar\n.\n.\n.\n8)Salir\n");
+    printf("Ingresa opcion:");
+
 }
-//void altas(char x[]){
-// printf("Ingresa el obj:");
-//  fgets(x,50, stdin);
-//  x[strcspn(x, "\n")] = '\0';
-//}
+void altas(char nombre[],int id[],int preorden[],int existencia[],float precio[],int total){
+    printf("Dame el nombre del producto:");
+    scanf("%s",&nombre[total]);
+    printf("Dame la existencia:");
+    scanf("%s",&existencia[total]);
+    printf("Dame el precio:");
+    scanf("%s",&precio[total]);
+    printf("Dame el punto de re-orden:");
+    scanf("%s",&preorden[total]);
+    id[total]=total+1;
+    printf("Alta exitosa!\n");
+}
 
 int main() {
-    int opt;
-    char obj[50];
-    menu();
-    printf("Ingresa opcion:");
-    scanf("%i",&opt);
-    do{
+    int opt,id[20],preorden[20],existencia[20],total=0;
+    float precio[20];
+    char nombre[20][25];
 
+
+    do{
+        menu();
+        scanf("%i",&opt);
         switch (opt) {
             case 1:
-                printf("-----Ingresaste a las altas-----\nRegistra Altas\n");
-                printf("%s", obj);
+                printf("-----Bienvenido al modulo de altas-----\n");
+                //altas(nombre,id,preorden,existencia,precio,total);
+                printf("Dame el nombre del producto:");
+                scanf("%s",&nombre[total]);
+                printf("Dame la existencia:");
+                scanf("%s",&existencia[total]);
+                printf("Dame el precio:");
+                scanf("%s",&precio[total]);
+                printf("Dame el punto de re-orden:");
+                scanf("%s",&preorden[total]);
+                id[total]=total+1;
+                printf("Alta exitosa!\n");
+                total++;
+                printf("%s\n",nombre[0]);
+                printf("%s\n",nombre[1]);
                 break;
             case 2:
-                printf("Ingresaste a listar\n");
+                printf("-----Listar----\n");
+                break;
+            case 8:
+                printf("Estas saliendo del progrma...");
                 break;
             default:
-                printf("Ingresaste una opcion incorrecta\n");
-                menu();
+                printf("<----->Ingresaste una opcion incorrecta<----->\n");
                 break;
         }
-        printf("\nPresiona Enter para continuar...");
-        getchar();
 
-        menu();
-        printf("Ingresa opcion:");
-        scanf("%i",&opt);
     } while (opt!=8);
-    printf("Estas saliendo del programa...");
+
     return 0;
 }
